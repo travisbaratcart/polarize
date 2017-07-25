3325225d51c812e6c7af34a7a52356a9657ac76d
Eveleen Sung <eveleensung@gmail.com>
2017-07-25 15:56:54 -0700

    add splitbar component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { Card } from './Card';
 export { ExampleComponent } from './ExampleComponent';
+export { Graph } from './Graph';
+export { Survey } from './Survey';
+export { SplitBar } from './SplitBar';
