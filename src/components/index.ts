650558f4ba1ffc6abf76aa025700d63974312b1b
Melinda Lim <melim@microsoft.com>
2017-07-25 15:09:09 -0700

    User data, counter component
    Adding the container for the counter and choices

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { ExampleComponent } from './ExampleComponent';
 export { Graph } from './Graph';
 export { Survey } from './Survey';
 export { SplitBar } from './SplitBar';
+export { Counter } from './Counter';
+export { CounterContainer } from './CounterContainer';
