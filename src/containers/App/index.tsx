998a0d16ca5678cf94df70f5d23b1e42f9e28a16
evels <eveleensung@gmail.com>
2017-07-25 11:26:00 -0700

    card and layout styles (#9)

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { RootState } from '../../reducers';
+import { ExampleComponent, Card } from '../../components';
 
 export namespace App {
   export interface Props extends RouteComponentProps<void> {
@@ -21,7 +22,17 @@ export class App extends React.Component<App.Props, App.State> {
   render() {
     return (
       <div>
-        <h1>This is a test</h1>
+        <div className="app-heading">
+          <h1>Pollarize</h1>
+        </div>
+        <div className="app-content">
+          <Card
+            cardNumber="1"
+            question="What is your question for this?"
+            description="The point of this question is to gauge your knowledge on nthis subject."
+            optionType="radio"
+            options={[{name:'asdasd',value:'asdasd'}]}/>
+        </div>
       </div>
     );
   }
